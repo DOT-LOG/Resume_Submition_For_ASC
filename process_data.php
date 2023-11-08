fa7604b5317eb3db39b7574b55f300323a524c9c
DotLog <149688925+DOT-LOG@users.noreply.github.com>
2023-11-08 11:59:21 +0100

    Update process_data.php

--- process_data.php
+++ process_data.php
@@ -2,17 +2,13 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $email = $_POST['email'];
-
-    // Save uploaded file
-    $resumeFile = $_FILES['resume']['tmp_name'];
-    $resumeFileName = $_FILES['resume']['name'];
-
+    
     // Email details
-    $to = "smlsml182@gmail.com"; // Change this to your email address
+    $to = "your@email.com"; // Change this to your email address
     $subject = "Resume Submission";
     $message = "Name: $name\nEmail: $email";
     $headers = "From: $email";
-
+    
     // Send the email
     if (mail($to, $subject, $message, $headers)) {
         // Email sent successfully
